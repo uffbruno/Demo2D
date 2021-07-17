5461b7ab49a5a4801f4b73399667289b3b93d826
Bruno Oliveira <uffbruno@gmail.com>
2021-07-17 11:15:31 -0300

    Created collectible sprite goldCoin

---
+++ collectible/collectible.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "collectible"
+  type: "sprite"
+  data: "tile_set: \"/collectible/collectible.atlas\"\n"
+  "default_animation: \"coinGold\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -1.0
+    y: -1.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
